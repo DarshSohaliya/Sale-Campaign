bfefee689d67d5d7de62382166c225f09bcd8de6
Darsh Sohaliya <darshsohaliya029290@gmail.com>
2025-04-28 17:44:33 +0530

    Change price according to campaign date

--- src/main/java/com/example/Sale/Campaign/Repository/CampaignRepository.java
+++ src/main/java/com/example/Sale/Campaign/Repository/CampaignRepository.java
@@ -2,6 +2,8 @@ package com.example.Sale.Campaign.Repository;
 
 import com.example.Sale.Campaign.Model.Campaign;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,7 @@ public interface CampaignRepository extends JpaRepository<Campaign,Long> {
     List<Campaign> findByStartDate(String startdate);
 
     List<Campaign> findByEndDate(String endate);
+
+    @Query("SELECT c FROM Campaign c JOIN c.campaignDiscounts cd WHERE cd.productId = :productId AND c.endDate > :today")
+    List<Campaign> findByProductIdAndEndDateAfter(@Param("productId") Long productId,@Param("today") String string);
 }
--- src/main/java/com/example/Sale/Campaign/Service/CampaignScheduler.java
+++ src/main/java/com/example/Sale/Campaign/Service/CampaignScheduler.java
@@ -14,7 +14,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class CampaignScheduler {
@@ -34,27 +36,57 @@ public class CampaignScheduler {
         LocalDate today = LocalDate.now();
         List<Campaign> campaigns = campaignRepository.findByStartDate(today.toString());
 
+        Map<Long,Double> productIdToMaxDiscount = new HashMap<>();
+
         for(Campaign campaign:campaigns){
             for(CampaignDiscount discount:campaign.getCampaignDiscounts()){
                 Product product = productRepository.findByProductId(Long.valueOf(discount.getProductId()));
 
-                if(product!=null){
-                    PriceHistory priceHistory = new PriceHistory();
+
+                Long productId = Long.valueOf(discount.getProductId());
+                double discountPercentage = discount.getDiscount();
+
+                if(!productIdToMaxDiscount.containsKey(productId) || discountPercentage > productIdToMaxDiscount.get(productId)){
+                     productIdToMaxDiscount.put(productId,discountPercentage);
+                }
+//                if(product!=null){
+//                    PriceHistory priceHistory = new PriceHistory();
+//                    priceHistory.setProductId(String.valueOf(product.getProductId()));
+//                    priceHistory.setOldPrice(product.getCurrentPrice());
+//                    priceHistory.setNewPrice(calculateNewPrice(product.getProductMRP(), product.getDiscount()));
+//                    priceHistory.setChangeDate(today.toString());
+//                    priceHistory.setNewDiscount(discount.getDiscount());
+//                    priceHistory.setOldDiscount(product.getDiscount());
+//                    pricehistoryRepository.save(priceHistory);
+//
+//
+//                    double newPrice = calculateNewPrice(product.getProductMRP() ,discount.getDiscount());
+//                    product.setCurrentPrice(newPrice);
+//                    product.setDiscount(discount.getDiscount());
+//                    productRepository.save(product);
+//
+//                }
+            }
+        }
+        for(Map.Entry<Long,Double> entry : productIdToMaxDiscount.entrySet()){
+            Long productId = entry.getKey();
+            double maxDiscount = entry.getValue();
+
+            Product product = productRepository.findByProductId(productId);
+            if(product != null){
+                PriceHistory priceHistory = new PriceHistory();
                     priceHistory.setProductId(String.valueOf(product.getProductId()));
                     priceHistory.setOldPrice(product.getCurrentPrice());
                     priceHistory.setNewPrice(calculateNewPrice(product.getProductMRP(), product.getDiscount()));
                     priceHistory.setChangeDate(today.toString());
-                    priceHistory.setNewDiscount(discount.getDiscount());
+                    priceHistory.setNewDiscount(maxDiscount);
                     priceHistory.setOldDiscount(product.getDiscount());
                     pricehistoryRepository.save(priceHistory);
 
-
-                    double newPrice = calculateNewPrice(product.getProductMRP() ,discount.getDiscount());
+                double newPrice = calculateNewPrice(product.getProductMRP() ,maxDiscount);
                     product.setCurrentPrice(newPrice);
-                    product.setDiscount(discount.getDiscount());
+                    product.setDiscount(maxDiscount);
                     productRepository.save(product);
-
-                }
             }
         }
     }
@@ -73,14 +105,41 @@ public class CampaignScheduler {
             for(CampaignDiscount discount : campaign.getCampaignDiscounts()){
                 System.out.println("###222");
                 Product product = productRepository.findByProductId(Long.valueOf(discount.getProductId()));
+
                 if(product != null){
-                    PriceHistory lastHistory = pricehistoryRepository.findTopByProductIdOrderByChangeDateDesc(String.valueOf(product.getProductId()));
-                    System.out.println("###333");
-                    if (lastHistory!=null){
-                        product.setCurrentPrice(lastHistory.getOldPrice());
-                        product.setDiscount(lastHistory.getOldDiscount());
+                    List<Campaign> activeCampaigns = campaignRepository.findByProductIdAndEndDateAfter(product.getProductId(),tody.toString());
+
+                    if(activeCampaigns != null && !activeCampaigns.isEmpty()){
+                        double maxDiscount = 0;
+                        for(Campaign activeCampaign : activeCampaigns){
+                            for(CampaignDiscount activeDiscont : activeCampaign.getCampaignDiscounts()){
+                                if(Long.valueOf(activeDiscont.getProductId()).equals(product.getProductId())){
+                                    if(activeDiscont.getDiscount() > maxDiscount){
+                                        maxDiscount = activeDiscont.getDiscount();
+                                    }
+                                }
+                            }
+                        }
+                        double newPrice = calculateNewPrice(product.getProductMRP(),maxDiscount);
+                        product.setCurrentPrice(newPrice);
+                        product.setDiscount(maxDiscount);
                         productRepository.save(product);
+                    }else{
+                        PriceHistory lastHistory = pricehistoryRepository.findTopByProductIdOrderByChangeDateDesc(String.valueOf(product.getProductId()));
+                        if(lastHistory !=null){
+                            product.setCurrentPrice(lastHistory.getOldPrice());
+                            product.setDiscount(lastHistory.getOldDiscount());
+                            productRepository.save(product);
+                        }
                     }
+
+//                    PriceHistory lastHistory = pricehistoryRepository.findTopByProductIdOrderByChangeDateDesc(String.valueOf(product.getProductId()));
+//                    System.out.println("###333");
+//                    if (lastHistory!=null){
+//                        product.setCurrentPrice(lastHistory.getOldPrice());
+//                        product.setDiscount(lastHistory.getOldDiscount());
+//                        productRepository.save(product);
+//                    }
                 }
             }
         }
